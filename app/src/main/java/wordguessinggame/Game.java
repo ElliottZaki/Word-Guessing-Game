596173efe4133001a319090fa760835cad43a69d
Elliott Zaki <elliottzaki@Elliotts-MacBook-Air.local>
2022-11-15 19:22:25 +0000

    Second Commit. Game class created. GetWordToGuess method created with two passing tests, that hides words starting from index [1] with an underscore.

--- app/src/main/java/wordguessinggame/Game.java
+++ app/src/main/java/wordguessinggame/Game.java
@@ -1,5 +1,34 @@
 package wordguessinggame;
 
 public class Game {
-    
+    String word;
+
+        Game(String chosenWord) {
+            word = chosenWord;
+        }
+    public static void main(String[] args) { }  
+
+    // public String getWordToGuess() {
+    //     StringBuilder hiddenWord = new StringBuilder(word); 
+    //         for (int i = 1; i < word.length(); i++) {
+    //             hiddenWord.replace(i, word.length(), "_");
+    //         } 
+    //     return hiddenWord.toString();
+    // }
+
+    // Debugging exercise 01: 
+    public String getWordToGuess() {
+        StringBuilder builder = new StringBuilder(word);
+            for (int i = 0; i < this.word.length(); i++) {
+                Character currentLetter = word.charAt(0);
+                if (i == 0) {
+                    builder.append(currentLetter);
+                } else {
+                    builder.replace(i, word.length(), ("_"));
+                }
+            }
+        return builder.toString();
+    }
 }
+
+
--- app/src/test/java/wordguessinggame/GameTest.java
+++ app/src/test/java/wordguessinggame/GameTest.java
@@ -4,8 +4,13 @@ import static org.junit.Assert.assertEquals;
 import org.junit.Test;
 
 public class GameTest {
-  @Test public void testGetsWordToGuess() {
-    Game game = new Game();
-    assertEquals(game.getWordToGuess(), "B_____");
+  @Test public void testsGetsWordMethod01() {
+    Game game = new Game("MAKERS");
+    assertEquals(game.getWordToGuess(), "M_____");
+  }
+
+  @Test public void testsGetsWordMethod02() {
+    Game game = new Game("JAVA");
+    assertEquals(game.getWordToGuess(), "J___");
   }
 }
